0629ab6d5b4e9e452970b2087358fa00851adb23
onticinc <onticinc@gmail.com>
2022-04-19 23:31:55 -0600

    got backend server working with frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [userName, setUsername] = useState('')
+
+  useEffect(() => {
+    blankRoute()
+  }, [])
+
+  const blankRoute = async () => {
+    const response = await axios.get('/blank');
+    console.log(response);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Front End</h1>
   );
 }
 
